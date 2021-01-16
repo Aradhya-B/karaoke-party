9d33e1ea52fbd1f8edf8f3cd670ad6a8de886bb9
Matthew <matthewdanics@gmail.com>
2021-01-16 17:49:22 -0500

    lrc viewer

---
+++ client/src/assets/music.js
@@ -0,0 +1,89 @@
+import BlindingLights from './songs/blinding_lights.mp3'
+
+export default [
+    {
+        id: 2,
+        cover: './source/2.jpeg',
+        singers: ['Cailin Russo', 'Chrissy Costanza'],
+        name: 'Phoenix',
+        src: BlindingLights,
+        lrc: `[00:13.93]Yeah
+[00:27.31]I've been tryna call
+[00:30.02]I've been on my own for long enough
+[00:32.82]Maybe you can show me how to love, maybe
+[00:38.46]I'm going through withdrawals
+[00:41.28]You don't even have to do too much
+[00:44.13]You can turn me on with just a touch, baby
+[00:49.67]I look around and
+[00:50.87]Sin City's cold and empty (Oh)
+[00:53.48]No one's around to judge me (Oh)
+[00:56.46]I can't see clearly when you're gone
+[01:00.82]I said, ooh, I'm blinded by the lights
+[01:06.93]No, I can't sleep until I feel your touch
+[01:12.13]I said, ooh, I'm drowning in the night
+[01:18.06]Oh, when I'm like this, you're the one I trust
+[01:22.48]Hey, hey, hey
+[01:34.62]I'm running out of time
+[01:37.32]'Cause I can see the sun light up the sky
+[01:40.24]So I hit the road in overdrive, baby, oh
+[01:46.98]The city's cold and empty (Oh)
+[01:49.70]No one's around to judge me (Oh)
+[01:52.49]I can't see clearly when you're gone
+[01:56.93]I said, ooh, I'm blinded by the lights
+[02:02.99]No, I can't sleep until I feel your touch
+[02:08.19]I said, ooh, I'm drowning in the night
+[02:14.20]Oh, when I'm like this, you're the one I trust
+[02:19.95]I'm just calling back to let you know (Back to let you know)
+[02:22.32]I could never say it on the phone (Say it on the phone)
+[02:25.69]Will never let you go this time (Ooh)
+[02:30.76]I said, ooh, I'm blinded by the lights
+[02:36.74]No, I can't sleep until I feel your touch
+[02:41.13]Hey, hey, hey
+[02:52.36]Hey, hey, hey
+[03:04.38]I said, ooh, I'm blinded by the lights
+[03:10.34]No, I can't sleep until I feel your touch`,
+    },
+    {
+        id: 1,
+        cover: './source/1.jpeg',
+        singers: ['陈蕾'],
+        name: '当我迷失时听着的歌',
+        src: BlindingLights,
+        lrc: `
+  [00:28.41]为着什么追赶到头来有变质吗
+  [00:35.34]日夜扮演开心角色吗 却总一个面对
+  [00:42.41]一个 流浪哪一街角找哪份寄托
+  [00:49.02]一个 闹市中却只得我
+  [00:55.49]这刹那却想到至亲的说话
+  [01:02.14]如若迷失请谨记回家
+  [01:06.19]迷路不要惊怕
+  [01:08.95]前路难关都给你招架
+  [01:12.83]如像孩子那样靠着我不怕
+  [01:19.72]好好的放一假
+  [01:23.76]重整理内心积压再出发
+  [01:36.13]望着父亲不经已白头发也斑斑
+  [01:42.66]家中每一位都尚好吗 我不敢致电你
+  [01:49.90]生怕 提及我的一切可会令你心挂
+  [01:56.77]生怕 说到声线变沙哑
+  [02:03.00]还未成功不敢说回家
+  [02:06.97]要再打拼一下
+  [02:09.79]同学们纷纷都已出嫁
+  [02:13.76]仍在留守这地 努力往上爬
+  [02:20.64]多少风雨洗刷
+  [02:24.60]还可有力气抵抗吗
+  [02:30.01]要记住回头看现状
+  [02:33.74]会发现已过三关斩了六将
+  [02:41.62]曾亦有灰色思绪在扩张
+  [02:46.45]你却是往日灿烂模样
+  [02:49.65]默默地为我在燃亮
+  [03:21.01]如若迷失请谨记回家
+  [03:24.91]迷路不再惊怕
+  [03:27.57]前路难关都给我招架
+  [03:31.50]犹幸能得你护荫 做你 亲手栽种的花
+  [03:39.34]若跌倒纵使满身伤痕沉沦在流沙
+  [03:46.08]我都不怕
+  [03:47.08]也得你的双手引路回家
+  [03:55.80]再都不怕
+  `,
+    },
+]
